54e832f0f6f2a166080adddf3309b68ab78efa60
Zhansaya Kozhdan <zhkozhdan09@gmail.com>
2024-06-13 02:28:46 +0300

    c

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIO(server, {
   cors: {
-    origin: 'http://localhost:3000', 
+    origin: 'https://hw2-chat-app-client.vercel.app/', 
     methods: ['GET', 'POST']
   }
 });
